c0449ce793cd4cfa1d0eb1bfadad27253694325f
Maximilian Trustworth <100371773+DonMassa84@users.noreply.github.com>
2023-10-03 01:49:46 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from database import initialize_database
+
+def main():
+    # Datenbank initialisieren
+    initialize_database()
+    
+    # Hauptlogik der Anwendung
+    # ...
+
+if __name__ == "__main__":
+    main()
