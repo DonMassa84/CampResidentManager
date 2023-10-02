69735dec164554f9b0260864b81a54858e81dc4c
Maximilian Trustworth <100371773+DonMassa84@users.noreply.github.com>
2023-10-03 01:57:26 +0200

    Update models.py

--- models.py
+++ models.py
@@ -1,52 +1,20 @@
 from datetime import datetime
 
 class CampBewohner:
-    def __init__(self, id, vorname, nachname, geburtsdatum, punkte=100):
+    def __init__(self, id, vorname, nachname, geburtsdatum, punkte):
         self.id = id
         self.vorname = vorname
         self.nachname = nachname
         self.geburtsdatum = geburtsdatum
         self.punkte = punkte
 
-    @property
-    def alter(self):
-        today = datetime.today()
-        return today.year - self.geburtsdatum.year - ((today.month, today.day) < (self.geburtsdatum.month, self.geburtsdatum.day))
-
 class RaumReservierung:
-    def __init__(self, reservierungs_id, camp_bewohner, raum_typ, startzeit, endzeit, anzahl_gaeste, zweck):
+    def __init__(self, reservierungs_id, camp_bewohner_id, raum_typ, startzeit, endzeit, anzahl_gaeste, zweck):
         self.reservierungs_id = reservierungs_id
-        self.camp_bewohner = camp_bewohner
+        self.camp_bewohner_id = camp_bewohner_id
         self.raum_typ = raum_typ
         self.startzeit = startzeit
         self.endzeit = endzeit
         self.anzahl_gaeste = anzahl_gaeste
         self.zweck = zweck
 
-class Spiel:
-    def __init__(self, spiel_id, name, fsk_altersfreigabe):
-        self.spiel_id = spiel_id
-        self.name = name
-        self.fsk_altersfreigabe = fsk_altersfreigabe
-
-class Film:
-    def __init__(self, film_id, titel, fsk_altersfreigabe):
-        self.film_id = film_id
-        self.titel = titel
-        self.fsk_altersfreigabe = fsk_altersfreigabe
-
-class PunkteTransaktion:
-    def __init__(self, transaktions_id, camp_bewohner, datum, punkte, grund):
-        self.transaktions_id = transaktions_id
-        self.camp_bewohner = camp_bewohner
-        self.datum = datum
-        self.punkte = punkte
-        self.grund = grund
-
-class Feedback:
-    def __init__(self, feedback_id, camp_bewohner, datum, kommentar, bewertung):
-        self.feedback_id = feedback_id
-        self.camp_bewohner = camp_bewohner
-        self.datum = datum
-        self.kommentar = kommentar
-        self.bewertung = bewertung
